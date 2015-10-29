53b75ec80a7367936f7916ce32cba830a0240959
SigurdurBaldursson <sigurdur14@i-7f3b5dd9.csd07cloud.internal>
2015-10-29 14:05:27 +0000

    Should run with spark

--- src/main/java/com/bangsapabbi/helloworld/HelloWorld.java
+++ src/main/java/com/bangsapabbi/helloworld/HelloWorld.java
@@ -1,8 +1,32 @@
 package com.bangsapabbi.helloworld;
 
+
+import static spark.Spark.*;
+
 public class HelloWorld {
+
+    
     public static void main(String[] args) {
-    World world = new World();    
-    System.out.println(world.greet());
+
+	World world = new World();    
+	//        Joke joke = client.getById(15);
+	String port = system.getenv("PORT");
+	if(port != null){
+	    setPort(Integer.valueOf(port));
+	}
+	   
+	get("/random",(req,res) -> world.getJoke());
+	
     }
+    
+    /*
+    public static Joke getj(){
+
+	return client.getById(15);
+    }
+    public static String getJokes(){
+	retun "asdf";
+    }
+    */
+    
 }
--- src/main/java/com/bangsapabbi/helloworld/World.java
+++ src/main/java/com/bangsapabbi/helloworld/World.java
@@ -1,10 +1,19 @@
 package com.bangsapabbi.helloworld;
+import net.joningi.icndb.ICNDBClient;
+import net.joningi.icndb.Joke;
 
 public class World {
+
+    final ICNDBClient client = new ICNDBClient();
 
     private String greeting = "Hello world!";
 
     public String greet() {
         return greeting;
+    
+    }
+    public String getJoke() {
+	World randomJoke = client.getRandom();
+	return randomJoke.getJoke();
     }
 }
